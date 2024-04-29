2bbe11adbb98be88a395d29d1c963ff590d91652
vitor paiva <paivavitorr@gmail.com>
2024-04-29 15:49:19 -0400

    ii

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,45 @@
 import Image from "next/image";
 import Logo from '@/assets/logo.svg';
-import Arrow from "@/assets/arrow-down.svg";
+import { ItemMenu } from "./ItemMenu";
+import { Search } from "./Search";
+import IconUser from '@/assets/icon-user.svg'
 
 export function Header(){
     return (
-        <header className="w-full h-20 bg-primary-orange">
-            <div>
-                <div>
-                    <div>
+        <header className="relative flex items-center w-full h-20 bg-primary-orange">
+            <div className="absolute top-0 right-0 bg-primary-blue w-[19%] h-full z-0"></div>
+            <div className="flex items-center justify-between w-full max-w-[1246px] px-[15px] mx-auto">
+                <div className="flex flex-1 items-center justify-between" >
+                    <div className="flex items-center gap-14">
                     <Image
                         src={Logo}
                         alt="logo"></Image>
-                        <ul>
+                        <ul className="flex items-center gap-14">
                             <li>
-                                <button>
-                                    <span>Para você</span>
-                                    <Image src={Arrow} alt="Arrow dropdown"></Image>
-                                </button>
+                               <ItemMenu
+                               name="Para você"/>
+                            </li>
+                            <li>
+                               <ItemMenu
+                               name="Para empresas"/>
+                            </li>
+                            <li>
+                               <ItemMenu
+                               name="Serviços"/>
+                            </li>
+                            <li>
+                               <ItemMenu
+                               name="Ajuda"/>
                             </li>
                         </ul>
                     </div>
+                   <Search/>
                 </div>
+                <button className="flex items-center gap-4 bg-primary-blue h-20 pl-10 z-10">
+                    <Image src={IconUser} alt="Icon user" />
+                   <span className="text-white font-bold">Acessar conta</span> 
+                   
+                </button>
             </div>
         </header>
     )
